ae953702d1bae0b03ec022461b1bfd341d8f4050
Vishnucharan <105539219+VishnucharanS@users.noreply.github.com>
2022-06-01 14:08:57 +0530

    Add files via upload

--- deltatask1.js
+++ deltatask1.js
@@ -1,5 +1,3 @@
-<script>
-function popup() {
-  alert("You clicked on the wrong tile");
+function rules() {
+  alert("General Rules: \n 1.The game is simple, you have to click on the tiles which blinks \n2. For each level there will be an increase in the number of tiles blinking \n3.The level which has all its tiles blinking is the last level \n4.You will see the specific rules of a mode once you choose the mode you want to play  ");
 }
-</script>
