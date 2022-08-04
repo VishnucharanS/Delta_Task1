041f5108cb286a631fa9d05503db05e92a948906
Vishnucharan <105539219+VishnucharanS@users.noreply.github.com>
2022-08-04 23:50:17 +0530

    Add files via upload

--- deltatask1.js
+++ deltatask1.js
@@ -1,4 +1,4 @@
-var r=''
+var lst = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'];
 var lst1 = []
 var lst2 = []
 var lst3 = []
@@ -14,7 +14,6 @@ function rules() {
   alert("General Rules: \n 1.The game is simple, you have to click on the tiles which blinks \n2. For each level there will be an increase in the number of tiles blinking \n3.The level which has all its tiles blinking is the last level \n4.You will see the specific rules of a mode once you choose the mode you want to play  ");
 }
 function randomnes(){
-  var lst = ["1","2",'3','4','5','6','7','8','9','10','11','12','13','14','15','16'];
   k = lst.length;
   while (k-- && k>=0) {
     l=Math.floor(Math.random()*(k));
@@ -48,9 +47,12 @@ function game(clic){
     for (i=0;i<17;i++){
       document.getElementById("g4"+i).classList.add("right")
     }
-    aud2.play()
+    aud2.play();
     setTimeout(function(){
       if (confirm("Woww!!!!!! You've finished it\nWanna play again?")) {
+        for (let i=0;i<lst.length;i++){
+          setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("wrong")},0);
+          setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("right")},0);}
         document.getElementById("4x4").style.display="grid";
         document.getElementById("start").style.display="inline";
         document.getElementById("score").style.display="none";
@@ -61,10 +63,12 @@ function game(clic){
         randomnes()
       } 
       else {
+        for (let i=0;i<lst.length;i++){
+          setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("wrong")},0);
+          setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("right")},0);}
         location.reload();
       }
       },4000);
-    location.reload()
   }
   if (lst3.length === 0){
     glowlist()
@@ -104,7 +108,9 @@ function game(clic){
       aud1.play();
       setTimeout(function(){
       if (confirm("Want to retry?")) {
-        setTimeout(function(){document.getElementById(smid).classList.remove("wrong")},0);
+        for (let i=0;i<lst.length;i++){
+          setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("wrong")},0);
+          setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("right")},0);}
         document.getElementById("4x4").style.display="grid";
         document.getElementById("start").style.display="inline";
         document.getElementById("score").style.display="none";
@@ -115,10 +121,12 @@ function game(clic){
         randomnes()
       } 
       else {
-        setTimeout(function(){document.getElementById(smid).classList.remove("wrong")},0);
+        for (let i=0;i<lst.length;i++){
+        setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("wrong")},0);
+        setTimeout(function(){document.getElementById("g4"+lst[i]).classList.remove("right")},0);}
         location.reload();
       }
-      },3000);
+    },3000);
     }
   }
 }
